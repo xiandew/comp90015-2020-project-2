0373e589417ef5e8512975a1d72253b251bea5da
Xiande Wen <visiande1998@gmail.com>
2020-05-28 15:08:04 +1000

    Integrate GUI with logic

--- src/client/GUI/GUI.java
+++ src/client/GUI/GUI.java
@@ -1,6 +1,5 @@
-package client;
+package client.GUI;
 
-import java.awt.EventQueue;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.util.Enumeration;
@@ -13,37 +12,27 @@ import javax.swing.JScrollPane;
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.border.LineBorder;
+
+import remote.ICollaborator;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class GUI {
 
 	private JFrame frmMain;
+	private Board panelWhiteBoard;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI window = new GUI();
-					window.frmMain.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public GUI() {
+	public GUI(ICollaborator c) {
+		this.panelWhiteBoard = new Board(c);
 		initialize();
 	}
 
-	public void showMessageDialog(String msg) {
+	public void launch() {
+		this.frmMain.setVisible(true);
+	}
+
+	public static void showMessageDialog(String msg) {
 		JOptionPane.showMessageDialog(null, msg);
 	}
 
@@ -57,7 +46,6 @@ public class GUI {
 		frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMain.getContentPane().setLayout(null);
 
-		Board panelWhiteBoard = new Board();
 		panelWhiteBoard.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panelWhiteBoard.setBounds(14, 13, 619, 551);
 		panelWhiteBoard.initCanvas();
@@ -90,47 +78,46 @@ public class GUI {
 		scrollPaneUsers.setBounds(14, 23, 282, 266);
 		panelUsers.add(scrollPaneUsers);
 
-		ButtonGroup buttonGroupBoardOperations = new ButtonGroup();
+		ButtonGroup buttonGroupBoardActions = new ButtonGroup();
 
-		JRadioButton rdbtnLine = new JRadioButton(Board.BoardOperation.LINE.toString());
+		JRadioButton rdbtnLine = new JRadioButton(Board.BoardAction.LINE.toString());
 		rdbtnLine.setSelected(true);
 		rdbtnLine.setBounds(14, 577, 61, 27);
 		frmMain.getContentPane().add(rdbtnLine);
-		buttonGroupBoardOperations.add(rdbtnLine);
+		buttonGroupBoardActions.add(rdbtnLine);
 
-		JRadioButton rdbtnCircle = new JRadioButton(Board.BoardOperation.CIRCLE.toString());
+		JRadioButton rdbtnCircle = new JRadioButton(Board.BoardAction.CIRCLE.toString());
 		rdbtnCircle.setBounds(81, 577, 77, 27);
 		frmMain.getContentPane().add(rdbtnCircle);
-		buttonGroupBoardOperations.add(rdbtnCircle);
+		buttonGroupBoardActions.add(rdbtnCircle);
 
-		JRadioButton rdbtnRect = new JRadioButton(Board.BoardOperation.RECT.toString());
+		JRadioButton rdbtnRect = new JRadioButton(Board.BoardAction.RECT.toString());
 		rdbtnRect.setBounds(164, 577, 61, 27);
 		frmMain.getContentPane().add(rdbtnRect);
-		buttonGroupBoardOperations.add(rdbtnRect);
+		buttonGroupBoardActions.add(rdbtnRect);
 
-		JRadioButton rdbtnText = new JRadioButton(Board.BoardOperation.TEXT.toString());
+		JRadioButton rdbtnText = new JRadioButton(Board.BoardAction.TEXT.toString());
 		rdbtnText.setBounds(231, 577, 61, 27);
 		frmMain.getContentPane().add(rdbtnText);
-		buttonGroupBoardOperations.add(rdbtnText);
+		buttonGroupBoardActions.add(rdbtnText);
 
-		JRadioButton rdbtnEraser = new JRadioButton(Board.BoardOperation.ERASER.toString());
+		JRadioButton rdbtnEraser = new JRadioButton(Board.BoardAction.ERASER.toString());
 		rdbtnEraser.setBounds(298, 577, 77, 27);
 		frmMain.getContentPane().add(rdbtnEraser);
-		buttonGroupBoardOperations.add(rdbtnEraser);
+		buttonGroupBoardActions.add(rdbtnEraser);
 
-		ActionListener selectBoardOperation = new ActionListener() {
+		ActionListener selectBoardAction = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				panelWhiteBoard.selectedOperation = Board.BoardOperation
-						.valueOf(((JRadioButton) e.getSource()).getText());
+				panelWhiteBoard.selectedAction = Board.BoardAction.valueOf(((JRadioButton) e.getSource()).getText());
 			}
 		};
 
 		// Bind button listener
-		Enumeration<AbstractButton> elements = buttonGroupBoardOperations.getElements();
+		Enumeration<AbstractButton> elements = buttonGroupBoardActions.getElements();
 		while (elements.hasMoreElements()) {
 			AbstractButton button = (AbstractButton) elements.nextElement();
-			button.addActionListener(selectBoardOperation);
+			button.addActionListener(selectBoardAction);
 		}
 	}
 }
