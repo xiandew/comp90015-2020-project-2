1c9623f17cb706e68bac93396b4d6fe3466bacda
Xiande Wen <visiande1998@gmail.com>
2020-05-29 23:08:27 +1000

    Reform the mediator with the manager logic

--- src/remote/IMediator.java
+++ src/remote/IMediator.java
@@ -2,16 +2,30 @@ package remote;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.UUID;
+import java.util.LinkedList;
 
 public interface IMediator extends Remote {
 	public static final String REMOTE_OBJECT_NAME = "SharedWhiteBord";
 
-	public UUID register(ICollaborator c) throws RemoteException;
+	public int register(ICollaborator c) throws RemoteException;
 
-	public boolean removeCollaborator(UUID id) throws RemoteException;
+	public int registerAsManager(ICollaborator m) throws RemoteException;
 
-	public boolean send(String data, UUID to, UUID from) throws RemoteException;
+	public LinkedList<String> getExecutedBoardActions() throws RemoteException;
 
-	public boolean broadcast(String data, UUID from) throws RemoteException;
+	public boolean removeUser(int id) throws RemoteException;
+
+	public LinkedList<String> getUsers() throws RemoteException;
+
+	public void send(String data, int to, int from) throws RemoteException;
+
+	public void broadcast(String data, int from) throws RemoteException;
+
+	public void broadcastAndAddBoardAction(String data, int from) throws RemoteException;
+
+	public void broadcastAndResetBoardActions(String data, int from) throws RemoteException;
+
+	public void resetBoardActions() throws RemoteException;
+
+	public void resetBoardActions(String boardActions) throws RemoteException;
 }
--- src/server/Mediator.java
+++ src/server/Mediator.java
@@ -7,14 +7,119 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
-import java.util.UUID;
+import java.util.LinkedList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import remote.ICollaborator;
 import remote.IMediator;
+import utils.ActionType;
 
 public class Mediator extends UnicastRemoteObject implements IMediator {
 	private static final long serialVersionUID = 1L;
-	private HashMap<UUID, ICollaborator> users;
+	private ICollaborator manager;
+	private HashMap<Integer, ICollaborator> users = new HashMap<>();
+	private int nUsers = 0;
+	private LinkedList<String> executedBoardActions = new LinkedList<>();
+
+	protected Mediator() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public int register(ICollaborator c) throws RemoteException {
+		if (this.manager == null) {
+			return -1;
+		}
+		int id = nUsers++;
+		this.users.put(id, c);
+		return id;
+	}
+
+	@Override
+	public int registerAsManager(ICollaborator m) throws RemoteException {
+		if (this.manager != null) {
+			return -1;
+		}
+		this.manager = m;
+		return this.register(m);
+	}
+
+	@Override
+	public LinkedList<String> getExecutedBoardActions() throws RemoteException {
+		return this.executedBoardActions;
+	}
+
+	@Override
+	public boolean removeUser(int id) throws RemoteException {
+		if (this.users.remove(id) == null) {
+			return false;
+		}
+		if (manager != null && id == manager.getId()) {
+			manager = null;
+			// Broadcast messages
+			JSONObject data = new JSONObject();
+			data.put("actionType", ActionType.MANAGER_EXIT.toString());
+			this.broadcastAndResetBoardActions(data.toString(), id);
+		}
+		return true;
+	}
+
+	@Override
+	public LinkedList<String> getUsers() throws RemoteException {
+		LinkedList<String> jUsers = new LinkedList<>();
+		for (ICollaborator user : this.users.values()) {
+			HashMap<String, String> jUser = new HashMap<>();
+			jUser.put("id", Integer.toString(user.getId()));
+			jUser.put("username", user.getUsername());
+			jUser.put("isManager", Boolean.toString(manager != null && user.getId() == manager.getId()));
+			jUsers.add(new JSONObject(jUser).toString());
+		}
+		return jUsers;
+	}
+
+	@Override
+	public void send(String data, int to, int from) throws RemoteException {
+		this.users.get(to).notify(data, from);
+	}
+
+	@Override
+	public void broadcast(String data, int from) throws RemoteException {
+		for (ICollaborator user : this.users.values()) {
+			try {
+				user.notify(data, from);
+			} catch (RemoteException e) {
+				// The user might be disconnected
+				this.removeUser(user.getId());
+			}
+		}
+	}
+
+	@Override
+	public void broadcastAndAddBoardAction(String data, int from) throws RemoteException {
+		this.executedBoardActions.add(data);
+		this.broadcast(data, from);
+	}
+
+	@Override
+	public void resetBoardActions() throws RemoteException {
+		this.executedBoardActions.clear();
+	}
+
+	@Override
+	public void resetBoardActions(String boardActions) throws RemoteException {
+		this.resetBoardActions();
+		for (Object jBoardAction : new JSONArray(boardActions)) {
+			this.executedBoardActions.add(jBoardAction.toString());
+		}
+	}
+
+	@Override
+	public void broadcastAndResetBoardActions(String data, int from) throws RemoteException {
+		this.resetBoardActions();
+		this.broadcast(data, from);
+	}
 
 	public static void main(String[] args) {
 
@@ -38,39 +143,5 @@ public class Mediator extends UnicastRemoteObject implements IMediator {
 			System.out.println("The remote object already bound");
 			System.exit(0);
 		}
-	}
-
-	protected Mediator() throws RemoteException {
-		this.users = new HashMap<>();
-	}
-
-	@Override
-	public UUID register(ICollaborator c) throws RemoteException {
-		UUID id = UUID.randomUUID();
-		this.users.put(id, c);
-		return id;
-	}
-
-	@Override
-	public boolean removeCollaborator(UUID id) throws RemoteException {
-		if (this.users.remove(id) == null) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public boolean send(String data, UUID to, UUID from) throws RemoteException {
-		return this.users.get(to).notify(data, from);
-	}
-
-	@Override
-	public boolean broadcast(String data, UUID from) throws RemoteException {
-		for (ICollaborator user : this.users.values()) {
-			if (!user.notify(data, from)) {
-				return false;
-			}
-		}
-		return true;
 	}
 }
