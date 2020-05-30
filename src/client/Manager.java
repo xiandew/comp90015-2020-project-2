c1b199c812d1a256a6fc25d935b307026aa285df
Xiande Wen <visiande1998@gmail.com>
2020-05-30 14:57:23 +1000

    Add notification approved event

--- src/client/Manager.java
+++ src/client/Manager.java
@@ -12,8 +12,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,8 +26,14 @@ public class Manager extends User {
 
 	public Manager(String username, String serverAddress, int serverPort) throws RemoteException {
 		super(username, serverAddress, serverPort);
+		this.gui.addManagerControls();
+		this.updateBoard();
+		this.updateUserList();
+		this.broadcastUserListUpdate();
+		this.launchGUI();
 	}
 
+	@Override
 	public void launchGUI() {
 		this.gui.mntmNew.addActionListener((ActionEvent ev) -> {
 			this.refreshBoard();
@@ -84,7 +88,67 @@ public class Manager extends User {
 			this.saveAs();
 		});
 
-		this.gui.launchAsManager();
+		this.gui.tableUsers.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent ev) {
+				int row = gui.tableUsers.rowAtPoint(ev.getPoint());
+				int col = gui.tableUsers.columnAtPoint(ev.getPoint());
+				int idCol = gui.tableUsers.getColumn("ID").getModelIndex();
+				int actionCol = gui.tableUsers.getColumn("Action").getModelIndex();
+				if (row >= 0 && col == actionCol && gui.tableUsers.getValueAt(row, col) == "Kick Out") {
+					int id = (int) gui.tableUsers.getValueAt(row, idCol);
+					String msg = String.format("You are going to kick out user %s", id);
+					if (JOptionPane.OK_OPTION == gui.showConfirmDialog(msg)) {
+						gui.modelUsers.removeRow(gui.tableUsers.convertRowIndexToModel(row));
+
+						// Use thread to prevent the blocking when sending messages
+						new Thread(() -> {
+							// Broadcast messages
+							JSONObject data = new JSONObject();
+							try {
+								data.put("actionType", ActionType.KICKED_OUT);
+								mediator.send(data.toString(), id, Manager.this.id);
+								mediator.removeUser(id);
+								data.put("actionType", ActionType.USER_LIST_UPDATE);
+								mediator.broadcast(data.toString(), Manager.this.id);
+							} catch (Exception e) {
+								// System.out.println("Unable to send messages");
+							}
+						}).start();
+					}
+				}
+			}
+		});
+
+		this.gui.tableNotifications.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent ev) {
+				int row = gui.tableNotifications.rowAtPoint(ev.getPoint());
+				int col = gui.tableNotifications.columnAtPoint(ev.getPoint());
+				int idCol = gui.tableNotifications.getColumn("UserID").getModelIndex();
+				int actionCol = gui.tableNotifications.getColumn("Action").getModelIndex();
+				if (row >= 0 && col == actionCol && gui.tableNotifications.getValueAt(row, col) == "Approve") {
+					int id = (int) gui.tableNotifications.getValueAt(row, idCol);
+					gui.modelNotifications.setValueAt("Approved", row, col);
+
+					new Thread(() -> {
+						// Broadcast messages
+						JSONObject data = new JSONObject();
+						try {
+							data.put("actionType", ActionType.JOIN_REQUEST_APPROVED);
+							mediator.send(data.toString(), id, Manager.this.id);
+							data.put("actionType", ActionType.USER_LIST_UPDATE);
+							mediator.broadcast(data.toString(), -1);
+						} catch (Exception e) {
+							gui.modelNotifications.setValueAt("N/A", row, col);
+							gui.showMessageDialog("Unable to approve the request. The user might have exited");
+						}
+					}).start();
+				}
+			}
+		});
+
+		this.gui.launch();
 	}
 
 	private void save() {
@@ -125,38 +189,26 @@ public class Manager extends User {
 	@Override
 	public void updateUserList() throws RemoteException {
 		this.gui.updateUserList(this.getUserList(), true);
-		this.gui.tableUsers.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent ev) {
-				int row = gui.tableUsers.rowAtPoint(ev.getPoint());
-				int col = gui.tableUsers.columnAtPoint(ev.getPoint());
-				int idCol = gui.tableUsers.getColumn("ID").getModelIndex();
-				int actionCol = gui.tableUsers.getColumn("Action").getModelIndex();
-				if (row >= 0 && col == actionCol && gui.tableUsers.getValueAt(row, col) == "Kick Out") {
-					int id = (int) gui.tableUsers.getValueAt(row, idCol);
-					String msg = String.format("You are going to kick out user %s", id);
-					if (JOptionPane.OK_OPTION == gui.showConfirmDialog(msg)) {
-						DefaultTableModel model = (DefaultTableModel) gui.tableUsers.getModel();
-						model.removeRow(gui.tableUsers.convertRowIndexToModel(row));
+	}
 
-						// Use thread to prevent the blocking when sending messages
-						new Thread(() -> {
-							// Broadcast messages
-							JSONObject data = new JSONObject();
-							try {
-								data.put("actionType", ActionType.KICKED_OUT);
-								mediator.send(data.toString(), id, Manager.this.id);
-								mediator.removeUser(id);
-								data.put("actionType", ActionType.USER_LIST_UPDATE);
-								mediator.broadcast(data.toString(), Manager.this.id);
-							} catch (Exception e) {
-								// System.out.println("Unable to send messages");
-							}
-						}).start();
-					}
-				}
-			}
-		});
+	@Override
+	public void notify(String data, int from) throws RemoteException {
+		super.notify(data, from);
+		if (from == this.id) {
+			return;
+		}
+
+		JSONObject jData = new JSONObject(data);
+		ActionType actionType = ActionType.valueOf((String) jData.get("actionType"));
+		switch (actionType) {
+		case JOIN_REQUEST:
+			String msgFmt = "User <[%s] %s> request to join the board";
+			String msg = String.format(msgFmt, jData.get("id"), jData.get("username"));
+			this.gui.modelNotifications.addRow(new Object[] { jData.get("id"), msg, "Approve" });
+			break;
+		default:
+			break;
+		}
 	}
 
 	public static void main(String[] args) {
